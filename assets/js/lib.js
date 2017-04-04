fc63ce72c480f626ac257f2e1e9b834acce2fd16
Lionz Hunter <hshowmail@gmail.com>
2017-04-04 10:46:39 +0000

    fix

--- assets/js/lib.js
+++ assets/js/lib.js
@@ -1,10 +1,13 @@
-var hzEmo = {
+var hzePopup = {
     all: [],
     url: "https://raw.githubusercontent.com/lionzhunter/hzWemoji/master/assets/js/emoji.json",
     MAX: 200,
-    UL: jQuery('.hz-emos ul'),
-    LIS: jQuery('.hz-emos ul').find('li'),
-    INPUT:  $('.hz-srch input'),
+    POPUPDOM: ".hz-emos",
+    SRCHDOM: '.hz-srch',
+    LOADING_CLASS: 'hz-loading',
+    UL: jQuery(this.POPUPDOM+' ul'),
+    LIS: jQuery(this.POPUPDOM+' ul').find('li'),
+    INPUT:  $(this.SRCHDOM+' input'),
     init: function(){
         this.INPUT.click(function(){
                 if($(this).css('margin-left')=='201px'){
@@ -16,20 +19,20 @@ var hzEmo = {
                 }
                    
             }).keyup(function(e){
-                hzEmo.clear()
-                    .put(hzEmo.search($(this).val()))
+                hzePopup.clear()
+                    .put(hzePopup.search($(this).val()))
                     .handlClick();
                 });
         return this;
     },
     loading: function(v){
         if(v==undefined){
-            return this.UL.hasClass('hz-loading')
+            return this.UL.hasClass(hzePopup.LOADING_CLASS)
         }else{
             if(v){
-                this.UL.addClass('hz-loading');
+                this.UL.addClass(hzePopup.LOADING_CLASS);
             }else{
-                this.UL.removeClass('hz-loading');
+                this.UL.removeClass(hzePopup.LOADING_CLASS);
             }
         }
         return this;
@@ -37,10 +40,10 @@ var hzEmo = {
     load: function(callback){
         this.loading(true);
         jQuery.getJSON(this.url, function(s) {
-            hzEmo.all = s;
-            hzEmo.loading(false)
+            hzePopup.all = s;
+            hzePopup.loading(false)
                 .put()
-                .handleClick();
+                .handlClick();
             if(callback!=undefined)  callback();
         });
         return this;
